a3cf954c8e705801baea5f69c8a09ca6e950048f
Ruslan <ruslanvs@yahoo.com>
2017-12-29 16:39:21 -0800

    Advanced assignment with some extras: random location of the enemies at deployment, easily added different types of enemies

--- script.js
+++ script.js
@@ -2,29 +2,33 @@ window.onload = mycode;
 function mycode () {
 
     var hero = {
-        x: 700,
-        y: 500
+        x: 500,
+        y: 700
     }
-    
+    var bullets = [];
+    var score = 0;
+    var offsetHero = 13;
+    var offsetBullets = 9;
+
     var enemies = []
     
     function enemiesDeployment () {
+        var max = 2; //the max numeric value of the enemy type name
+        var min = 1; // ----- // -----
+        
         for ( var i = 0; i < 10; i++) {
             enemies.push ( {
-                x: Math.random () * 1000,
+                c: "enemy" + Math.floor(Math.random()*(max-min+1)+min) + "",
+                x: Math.floor(Math.random()*(1000-100+1)+100),
                 y: Math.random () * 100,
                 s: 2 + Math.random () * .5
             } )
+            if (enemies[i].c == "enemy1") {
+                enemies[i].offset = 14;
+            } else enemies[i].offset = 6;
         }
     }
     enemiesDeployment ();
-    console.log ( enemies );
-
-    var bullets = [];
-    var score = 0;
-    var offsetEnemies = 14;
-    var offsetBullets = 9;
-
 
     function displayHero () {
         document.getElementById( 'hero' ).style[ 'left' ] = hero.x + "px";
@@ -34,7 +38,7 @@ function mycode () {
     function displayEnemies () {
         var output = "";
         for (var i = 0; i < enemies.length; i++) {
-            output += "<div class='enemy1' style='top:" + enemies[i].y + "px; left:" + enemies[i].x + "px;'></div>";
+            output += "<div class='" + enemies[i].c + "' style='top:" + enemies[i].y + "px; left:" + enemies[i].x + "px;'></div>";
         }
         document.getElementById( "enemies" ).innerHTML = output;
     }
@@ -51,20 +55,12 @@ function mycode () {
         document.getElementById( "score" ).innerHTML = score;
     }
 
-    // function displayIndicator () {
-    //     var output = "";
-    //     for (var i = 0; i < enemies.length; i++) {
-    //         output += "<div class='indicators' style='top:" + enemies[i].y + "px; left:" + enemies[i].x + "px;'></div>";
-    //     }
-    //     document.getElementById( "indicator" ).innerHTML = output;
-    // }
-    
     function moveEnemies () {
         for (var i = 0; i < enemies.length; i++) {
             enemies[i].y += enemies[i].s;
-            if ( enemies[i].y > 800) {
+            if ( enemies[i].y > 800 ) {
                 enemies[i].y = 0;
-                enemies[i].x = Math.random () * 1000;
+                enemies[i].x = Math.random () * (1000-100+1)+100;
                 enemies[i].s = 2 + Math.random () * .5;
             }
         }
@@ -77,33 +73,61 @@ function mycode () {
                 bullets[i] = bullets[bullets.length - 1];
                 bullets.pop ();
             }
-            // console.log ( bullets );
         }
     }
-    
+
     function gameLoop () {
         displayHero ();
         moveEnemies ();
         displayEnemies ();
-        // displayIndicator ();
         moveBullets ();
         displayBullets ();
+        // displayIndicator ();
         detectCollision ();
         displayScore ();
     }
 
-    setInterval( gameLoop, 15 );
+    function delayedLoop () {
+        removeExploded ();
+    }
+
+    setInterval( gameLoop, 20 );
+    setInterval( delayedLoop, 200 );
+
+    // function displayIndicator () {
+    //     var output = "";
+    //     for (var i = 0; i < enemies.length; i++) {
+    //         output += "<div class='indicators' style='top:" + enemies[i].y + enemies[i].offset + 20 + "px; left:" + enemies[i].x + enemies[i].offset + "px;'></div>";
+    //     }
+    //     document.getElementById( "indicator" ).innerHTML = output;
+    // }
 
     function detectCollision () {
-        for (var i = 0; i < bullets.length; i++) {
-            for (var j = 0; j < enemies.length; j++) {
-                if( Math.abs( (bullets[i].x + offsetBullets ) - (enemies [j].x + offsetEnemies)) < 5 && Math.abs( (bullets[i].y - offsetBullets) - (enemies [j].y - offsetEnemies)) < 6 ) {
+        for (var i = 0; i < enemies.length; i++) {
+            for (var j = 0; j < bullets.length; j++) {
+                if( Math.abs( (bullets[j].x + offsetBullets ) - (enemies[i].x + enemies[i].offset)) < 5 && Math.abs( (bullets[j].y + offsetBullets) - ( enemies [i].y + enemies[i].offset )) < 3 ) {
                     score += 10;
-                    console.log ( "hit! " );
-                    console.log ( 'bullet', i, 'hit enemy', j);
+                    console.log ( 'bullet', j, 'hit enemy', i);
+                    enemies[i].c = "enemyExplode";
+                    enemies[i].s = 1;
+                    console.log ( enemies );
                 }
+                
+            }
+            if( Math.abs( ( hero.x + offsetHero ) - (enemies[i].x + enemies[i].offset)) < ( offsetHero + enemies[i].offset ) && Math.abs( (hero.y + offsetHero) - (enemies [i].y + enemies[i].offset)) < ( 1 ) ) {
+                score -= 500;
+                console.log ( 'hero hit enemy', i);
             }
         }
+    }
+
+    function removeExploded () {
+            for (var i = 0; i < enemies.length; i++) {
+                if ( enemies[i].c == "enemyExplode" ) {
+                    enemies[i] = enemies[enemies.length - 1];
+                    enemies.pop ();
+                }
+            }
     }
 
     document.onkeydown = function(a) {
@@ -121,7 +145,6 @@ function mycode () {
         }
         if ( a.keyCode == 32 ) { //spacebar
             bullets.push ({x: hero.x + 6, y: hero.y - 13 });
-            // displayBullets ();
         }
         displayHero ();
     }
